9760612640347cb6f2cf1b17fbbeabd38c4d93ea
Louis-Solirius <louis.ware@solirius.com>
2025-02-19 23:57:44 +0000

    Add bastion host, bastion subnet, and bastion public IP

--- terraform/network.tf
+++ terraform/network.tf
@@ -12,34 +12,51 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = "bastion-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "example-bastion"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  tunneling_enabled   = true
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
+
 resource "azurerm_network_security_group" "vm_nsg" {
   name                = "ansible-app-server-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "ssh-allow"
+    name                       = "ssh-allow-from-bastion"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_address_prefix      = "*"
+    source_address_prefix      = "10.0.2.0/24"  # <- Bastion subnet range
     source_port_range          = "*"
     destination_address_prefix = "*"
     destination_port_range     = "22"
   }
-
-  # security_rule {
-  #   name                       = "ssh-allow2"
-  #   priority                   = 1002
-  #   direction                  = "Inbound"
-  #   access                     = "Allow"
-  #   protocol                   = "Tcp"
-  #   source_address_prefix      = var.trusted_ip2
-  #   source_port_range          = "*"
-  #   destination_address_prefix = "*"
-  #   destination_port_range     = "22"
-  # }
 
   security_rule {
     name                       = "8080-allow"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,13 @@ output "public_ip" {
   description = "Public IP of the VM"
   value       = azurerm_public_ip.vm_public_ip.ip_address
 }
+
+output "bastion_host_name" {
+  description = "Name of the Bastion Host"
+  value = azurerm_bastion_host.bastion.name
+}
+
+output "bastion_host_ip" {
+  description = "Public IP of the Bastion Host"
+  value = azurerm_public_ip.bastion_public_ip.ip_address
+}
